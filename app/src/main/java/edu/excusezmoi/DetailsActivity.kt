45d69f021a56d4af435be91fc129616f090d68d2
Gregurevic <paul.greg.indy@gmail.com>
2022-04-10 12:00:05 +0200

    added Hilt related objects to project

---
+++ app/src/main/java/edu/excusezmoi/DetailsActivity.kt
@@ -0,0 +1,18 @@
+package edu.excusezmoi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.excusezmoi.ui.details.DetailsFragment
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.details_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, DetailsFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
