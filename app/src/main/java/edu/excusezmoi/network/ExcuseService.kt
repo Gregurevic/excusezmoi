49448be8f977d6b54b0e313a702ab28140fd39e5
Gregurevic <paul.greg.indy@gmail.com>
2022-04-20 20:03:27 +0200

    added API calls and their description to the project repo

--- app/src/main/java/edu/excusezmoi/network/ExcuseService.kt
+++ app/src/main/java/edu/excusezmoi/network/ExcuseService.kt
@@ -1,11 +1,26 @@
 package edu.excusezmoi.network
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ExcuseService {
 
     @GET("excuse")
     suspend fun getExcuse(): ExcuseNetworkEntity
 
-    ///...
+    @GET("excuse/{count}")
+    suspend fun getExcuses(
+        @Path("count") count: Int
+    ): List<ExcuseNetworkEntity>
+
+    @GET("excuse/{category}")
+    suspend fun getExcuseByCategory(
+        @Path("category") category: String
+    ): ExcuseNetworkEntity
+
+    @GET("excuse/{category}/{count}")
+    suspend fun getExcusesByCategory(
+        @Path("category") category: String,
+        @Path("count") count: Int
+    ): List<ExcuseNetworkEntity>
 }
